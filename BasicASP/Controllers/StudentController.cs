e02f64d1ea80b991454303c9f395fe51817092a8
YhongYhong <prommmm44@gmail.com>
2024-02-29 02:07:08 +0700

    "first commit"

---
+++ BasicASP/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using BasicASP.Data;
+using BasicASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public StudentController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Student> s1 = _db.Students;
+            return View(s1);
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(Student obj)
+        {
+            var isEmailExisted = _db.Students.Any(s => s.Email == obj.Email);
+            if (ModelState.IsValid && !isEmailExisted)
+            {
+                _db.Students.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            if (isEmailExisted){ ModelState.AddModelError("Email", "Email already used."); }
+            return View(obj);
+        }
+        public IActionResult ShowScore(int id)
+        {
+            return Content($"คะแนนของเลขที่ {id}");
+        }
+    }
+}
---
+++ BasicASP/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicASP.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirmation Password is required.")]
+        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
